3acd5de7a4e9a365b61e9543f41b277e8818918a
middletaq@gmail.com <middletaq@gmail.com>
2023-04-04 22:10:40 -0400

    #16

---
+++ training #16.js
@@ -0,0 +1,26 @@
+// Task
+// Coding in function cutIt, function accept 1 parameter:arr. arr is a string array.
+
+// The first mission: Traversing arr, find the shortest string length.
+
+// The second mission: Traversing arr again, intercept all strings to the shortest string length(Start from index0). you can use one of slice() substring() or substr() do it. return the result after finished the work.
+
+function cutIt(arr){
+ let shortLength = arr[0].length;
+  
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i].length < shortLength) {
+      shortLength = arr[i].length;
+    }
+  }
+for (let i = 0; i < arr.length; i++) {
+    arr[i] = arr[i].slice(0, shortLength);
+  }
+  
+  return arr;
+}
+// arr is a string array
+// First loop arr to find the shortest string length
+// Second loop arr again and find the shortest string length start from index0
+// use slice() or substring (), substr()
+// return the result
