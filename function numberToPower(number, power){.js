29f0609fdaada01b7b0990833b068c2971544300
middletaq@gmail.com <middletaq@gmail.com>
2023-04-11 22:03:30 -0400

    power

---
+++ function numberToPower(number, power){.js
@@ -0,0 +1,93 @@
+function numberToPower(number, power){
+    let total = 1
+    for(let i = 0; i < power; i++){
+      total *= number
+    }
+    return total
+      
+  }
+
+// 1 
+
+function numberToPower(number, power){
+    let total = 1
+    for(let i = 0; i < power; i++){
+        total *= number
+    }
+    return total
+}
+
+
+// 2
+function numberToPower(number, power){
+    let total = 1
+    for(let i = 0; i < power; i++){
+        total *= number
+    }
+    return total
+}
+// 3
+function numberToPower(number, power){
+    let total = 1
+    for(let i = 0; i < power; i++){
+        total *= number
+    }
+    return total
+
+}
+//  4
+function numberToPower(number, power){
+    let total = 1
+    for(let i = 0; i < power; i++){
+        total *= number
+    }
+    return total
+}
+// 5
+function numberToPower(number, power){
+    let total = 1
+    for(let i = 0; i < power; i++){
+        total *= number
+    }
+    return total
+}
+// 6
+function numberToPower(number, power){
+    let total = 1
+    for(let i = 0; i < power; i++){
+        total *= number
+    }
+    return total
+}
+// 7
+function numberToPower(number, power){
+    let total = 1
+    for(let i = 0; i < power; i++){
+        total *= number
+    }
+    return total
+}
+// 8
+function numberToPower(number, power){
+    let total = 1
+    for(let i = 0; i < power; i++){
+        total *= number
+    }
+    return total
+}
+// 9
+function numberToPower(number,power){
+    let total = 1
+    for(let i = 0; i < power; i++){
+        total *= number
+    }
+    return total
+}
+// 10
+function numberToPower(number, power){
+    let total = 1
+    for(let i = 0; i < power; i++){
+        total *= number
+    }
+    return total
+}
