2ab4828e6b851fe1e2f0c2bba7243be0e5c669e5
middletaq@gmail.com <middletaq@gmail.com>
2023-04-27 23:05:27 -0400

    vowels

---
+++ vowels 7kyu.js
@@ -0,0 +1,34 @@
+
+// Return the number (count) of vowels in the given string.
+
+// We will consider a, e, i, o, u as vowels for this Kata (but not y).
+
+// The input string will only consist of lower case letters and/or spaces
+
+
+
+function getCount(str) {
+  
+  let count = 0
+  for(let i = 0; i < str.length; i++){
+    if(str[i] === 'a' || str[i] === 'e' || str[i] === 'i' || str[i] === 'o' || str[i] === 'u'){
+      count += 1
+    }
+  }
+  
+  return count;
+}
+
+// 1
+
+// 2
+
+// 3
+
+// 4
+// 5
+// 6
+// 7
+// 8
+// 9
+// 10
