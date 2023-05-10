2846e33b10e76234be1b5fd616addbf928b4e6ed
middletaq@gmail.com <middletaq@gmail.com>
2023-05-10 16:22:07 -0400

    lost

---
+++ lost-without-map.js
@@ -0,0 +1,36 @@
+// Given an array of integers, return a new array with each value doubled.
+
+// For example:
+
+// [1, 2, 3] --> [2, 4, 6]
+
+
+function maps(x){
+    return x.map(n => n * 2);
+    }
+
+// 1
+function maps(x){
+    return x.map(n => n * 2)
+}
+
+// 2
+function maps(x){
+    return x.map(n => n * 2)
+}
+
+// 3
+function maps(x){
+    return x.map(n => n * 2)
+}
+
+// 4
+function maps(x){
+    return x.map(n => n * 2)
+}
+
+// 5
+function maps(x){
+    return x.map(n => n * 2)
+}
+
